894b6671f740dc73e83953703b9db5e1efec76b0
Sairam Srihari Manish Subrahmanya Ganti <sairamg@boxelder.rlogin>
2022-05-06 04:51:37 -0400

    Power 8 stats for memory type LPDDR3 1600 MHz

--- power8.py
+++ power8.py
@@ -118,7 +118,7 @@ if m5.defines.buildEnv['TARGET_ISA'] == "x86":
 
 # Create a DDR3 memory controller and connect it to the membus
 system.mem_ctrl = MemCtrl()
-system.mem_ctrl.dram = DDR3_2133_8x8()
+system.mem_ctrl.dram = LPDDR3_1600_1x32()
 system.mem_ctrl.dram.range = system.mem_ranges[0]
 system.mem_ctrl.port = system.membus.mem_side_ports
 
@@ -134,7 +134,7 @@ thispath = os.path.dirname(os.path.realpath(__file__))
 print (thispath)
 
 #binary = os.path.join(thispath, '../../tests/test-progs/matrix_multiply.out')
-binary = os.path.join(thispath, 'radix_sort_5x1000.out')
+binary = os.path.join(thispath, 'radix_sort_5x100000.out')
 print (binary)
 
 system.workload = SEWorkload.init_compatible(binary)
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-../gem5_workspace/gem5/build/POWER/gem5.opt -d statsPower8/DDR3_2133stats_5x1000 power8.py
+../gem5_workspace/gem5/build/POWER/gem5.opt -d statsPower8/LPDDR3_1600stats_5x100000 power8.py
